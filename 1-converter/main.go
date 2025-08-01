7c5d4c456399636e57fd00a13507f1c388b670c4
Bogdan Klimov <biklimov@avito.ru>
2025-08-01 22:52:42 +0300

    homework 3-2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,49 +1,71 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
-	amount, initialCurrency, targetCurrency, err := getUserInput()
-
-	if err != nil {
-		fmt.Printf("Ошибка: %v", err)
-		return
-	}
+	initialCurrency := getInitialCurrency()
+	amount := getAmount()
+	targetCurrency := getTargetCurrency(initialCurrency)
 
 	result := calculateTarget(amount, initialCurrency, targetCurrency)
 	fmt.Printf("%v %v = %.2f %v", amount, initialCurrency, result, targetCurrency)
 }
 
-func getUserInput() (float64, string, string, error) {
-	var amount float64
+func getInitialCurrency() string {
 	var initialCurrency string
-	var targetCurrency string
-
-	fmt.Printf("Введите исходную валюту (USD, EUR, RUB): ")
+	fmt.Print("Введите исходную валюту (USD, EUR, RUB): ")
 	fmt.Scan(&initialCurrency)
 
-	if initialCurrency != "USD" && initialCurrency != "EUR" && initialCurrency != "RUB" {
-		return 0, "", "", errors.New("CURRENCY_ERROR")
+	for {
+		if initialCurrency != "USD" && initialCurrency != "EUR" && initialCurrency != "RUB" {
+			fmt.Println("Не подходящая валюта, попробуйте снова")
+			fmt.Print("Введите исходную валюту (USD, EUR, RUB): ")
+			fmt.Scan(&initialCurrency)
+			continue
+		}
+		break
 	}
 
-	fmt.Printf("Введите число: ")
-	fmt.Scan(&amount)
+	return initialCurrency
+}
 
-	if amount <= 0 {
-		return 0, "", "", errors.New("MISSING_AMOUNT")
-	}
+func getTargetCurrency(initialCurrency string) string {
+	var targetCurrency string
 
-	fmt.Printf("Введите целевую валюту %v: ", getCurrencyHint(initialCurrency))
+	fmt.Printf("Введите исходную валюту %v: ", getCurrencyHint(initialCurrency))
 	fmt.Scan(&targetCurrency)
 
-	if initialCurrency != "USD" && initialCurrency != "EUR" && initialCurrency != "RUB" || targetCurrency == initialCurrency {
-		return 0, "", "", errors.New("CURRENCY_ERROR")
+	for {
+		if (targetCurrency != "USD" && targetCurrency != "EUR" && targetCurrency != "RUB") || targetCurrency == initialCurrency {
+			fmt.Println("Не подходящая валюта, попробуйте снова")
+			fmt.Printf("Введите исходную валюту %v: ", getCurrencyHint(initialCurrency))
+			fmt.Scan(&targetCurrency)
+			continue
+		}
+		break
 	}
 
-	return amount, initialCurrency, targetCurrency, nil
+	return targetCurrency
+}
+
+func getAmount() float64 {
+	var amount float64
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+
+	for {
+		if amount <= 0 {
+			fmt.Println("Сумма неверная, попробуйте снова")
+			fmt.Print("Введите сумму: ")
+			fmt.Scan(&amount)
+			continue
+		}
+		break
+	}
+
+	return amount
 }
 
 func getCurrencyHint(initial string) string {
@@ -63,20 +85,20 @@ func calculateTarget(amount float64, initialCurrency string, targetCurrency stri
 	const USD_RUB = 81.25
 
 	if initialCurrency == "USD" {
-		if (targetCurrency == "EUR") {
+		if targetCurrency == "EUR" {
 			return amount * USD_EUR
 		}
 		return amount * USD_RUB
 	}
 
 	if initialCurrency == "EUR" {
-		if (targetCurrency == "USD") {
+		if targetCurrency == "USD" {
 			return amount / USD_EUR
 		}
 		return amount / USD_EUR * USD_RUB
 	}
 
-	if (targetCurrency == "USD") {
+	if targetCurrency == "USD" {
 		return amount / USD_RUB
 	}
 	return amount / USD_RUB * USD_EUR
